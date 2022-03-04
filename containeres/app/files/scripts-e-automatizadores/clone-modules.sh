3db2328d5a6c2ad09348b087709d75f79b0cb72c
Marcelo Linhares Castro <marlinhares@gmail.com>
2022-03-04 18:30:23 -0300

    Adicionando o módulo de Gestao Documental como opcional na instalacao

--- containeres/app/files/scripts-e-automatizadores/clone-modules.sh
+++ containeres/app/files/scripts-e-automatizadores/clone-modules.sh
@@ -7,3 +7,6 @@ cd /sei-modulos
 git clone https://github.com/spbgovbr/mod-sei-estatisticas.git
 git clone https://github.com/spbgovbr/mod-wssei.git
 git clone https://github.com/spbgovbr/mod-sei-resposta.git
+git clone https://github.com/spbgovbr/mod-gestao-documental.git
+#git clone https://github.com/spbgovbr/mod-sei-loginunico.git
+#git clone https://github.com/spbgovbr/mod-sei-assinatura-avancada.git
