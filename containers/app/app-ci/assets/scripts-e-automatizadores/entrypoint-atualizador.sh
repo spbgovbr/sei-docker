dbab37fedaab84747d29cc853dab038a36998547
Marcelo L Castro <marlinhares@gmail.com>
2024-02-23 14:40:09 -0300

    Corrigindo bug 74, quando multiorgaos instalador gerava erro ao rodar novamente

--- containers/app/app-ci/assets/scripts-e-automatizadores/entrypoint-atualizador.sh
+++ containers/app/app-ci/assets/scripts-e-automatizadores/entrypoint-atualizador.sh
@@ -173,63 +173,24 @@ echo "UPDATE NA BASE DE DADOS - ORGAO E SISTEMA**********"
 echo "***************************************************"
 echo "***************************************************"
 
-# Atualização do endereço de host da aplicação
-echo "Atualizando Banco de Dados com as Configuracoes Iniciais..."
-if [ "$APP_DB_TIPO" == "MySql" ]; then
-    echo "Atualizando MySql..."
-    MYSQL_CMD="mysql --host $APP_DB_HOST --user $APP_DB_ROOT_USERNAME --password=$APP_DB_ROOT_PASSWORD"
-    $MYSQL_CMD -e "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" sip
-    $MYSQL_CMD -e "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" sei
-    $MYSQL_CMD -e "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP';" sip
-    $MYSQL_CMD -e "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI';" sip
-fi
 
-if [ "$APP_DB_TIPO" == "Oracle" ]; then
-    echo "Atualizando Oracle..."
-    echo "alter user sip identified by sip_user;" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "alter user sei identified by sei_user;" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" | sqlplus64 sei/sei_user@$APP_DB_HOST
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP';" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI';" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-fi
+if [ ! -f /sei/controlador-instalacoes/instalado.ok ]; then
+	# Atualização do endereço de host da aplicação
+	echo "Atualizando Banco de Dados com as informacoes de orgao e sistema..."
 
-if [ "$APP_DB_TIPO" == "SqlServer" ]; then
-    echo "Atualizando SqlServer..."
-    echo "use sip" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_ROOT_USERNAME -P $APP_DB_ROOT_PASSWORD
+	php -r "
+	require_once '/opt/sip/web/Sip.php';
+	\$conexao = BancoSip::getInstance();
+	\$conexao->abrirConexao();
+	\$conexao->executarSql(\"update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO'\");
+	\$conexao->executarSql(\"update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP'\");
+	\$conexao->executarSql(\"update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI'\");"
 
-
-    echo "use sei" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_SEI_USERNAME -P $APP_DB_SEI_PASSWORD
-
-    echo "use sip" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_ROOT_USERNAME -P $APP_DB_ROOT_PASSWORD
-
-    echo "use sip" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_ROOT_USERNAME -P $APP_DB_ROOT_PASSWORD
-fi
-
-if [ "$APP_DB_TIPO" == "PostgreSql" ]; then
-    echo "Atualizando Postgres..."
-
-    PSQL_CMD="psql -h $APP_DB_HOST -U $APP_DB_ROOT_USERNAME "
-    PGPASSWORD=$APP_DB_ROOT_PASSWORD $PSQL_CMD -d sip -c "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';"
-    PGPASSWORD=$APP_DB_ROOT_PASSWORD $PSQL_CMD -d sei -c "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';"
-    PGPASSWORD=$APP_DB_ROOT_PASSWORD $PSQL_CMD -d sip -c "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP';"
-    PGPASSWORD=$APP_DB_ROOT_PASSWORD $PSQL_CMD -d sip -c "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI';"
+	php -r "
+	require_once '/opt/sei/web/SEI.php';
+	\$conexao = BancoSEI::getInstance();
+	\$conexao->abrirConexao();
+	\$conexao->executarSql(\"update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO'\");"
 fi
 
 echo "***************************************************"
@@ -281,21 +242,22 @@ cp sei-ca.pem /etc/pki/ca-trust/source/anchors/
 update-ca-trust extract
 update-ca-trust enable
 
-# multiorgaos
-echo "Vamos verificar se passou multiorgaos para instalar"
-if [ "$APP_ORGAOS_ADICIONAIS_SIGLA" == "" ] || [ "$APP_ORGAOS_ADICIONAIS_NOME" == "" ]; then
-	echo "Arrays multiorgaos vazios ou preenchidos incorretamente, pulando instalacao multiorgaos"
-else
-
-	/usr/sbin/httpd -DFOREGROUND &
-
-	echo "Vamos executar agora o script para cadastrar multiorgaos"
-	php /sei/files/scripts-e-automatizadores/misc/cadastrarMultiorgao.php
-	echo "Script multiorgaos executado verifique se houve observacoes acima"
-
-fi
-
 if [ ! -f /sei/controlador-instalacoes/instalado.ok ]; then
+	
+	# multiorgaos
+	echo "Vamos verificar se passou multiorgaos para instalar"
+	
+	if [ "$APP_ORGAOS_ADICIONAIS_SIGLA" == "" ] || [ "$APP_ORGAOS_ADICIONAIS_NOME" == "" ]; then
+	    echo "Arrays multiorgaos vazios ou preenchidos incorretamente, pulando instalacao multiorgaos"
+    else
+
+	    /usr/sbin/httpd -DFOREGROUND &
+
+	    echo "Vamos executar agora o script para cadastrar multiorgaos"
+	    php /sei/files/scripts-e-automatizadores/misc/cadastrarMultiorgao.php
+	    echo "Script multiorgaos executado verifique se houve observacoes acima"
+
+    fi
 
     echo "Vamos fazer o apache se apropriar dos dados externos... Aguarde"
     chown -R apache:apache /sei/arquivos_externos_sei/
